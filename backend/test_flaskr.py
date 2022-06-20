f8fc56614bb8bd714ddad412ed68b4afb4fea698
unknown <greatgrant116@gmail.com>
2022-06-20 17:43:23 +0100

    tested delete endpoint

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -54,7 +54,16 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data["success"])
         self.assertTrue(data["categories"])
         self.assertEqual(data["total_categories"], 6)
+    
+    def test_delete_question(self):
+        response = self.client().delete("questions/23")
+        data = json.loads(response.data)
 
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertEqual(data["deleted"], 23)
+        self.assertTrue(data["questions"])
+        self.assertTrue(data["total_questions"])
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
